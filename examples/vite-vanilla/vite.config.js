ae541a8d04240b60e4561619123217d219f54000
metaory <metaory@gmail.com>
2025-04-11 23:00:13 +0330

    vite fix

--- examples/vite-vanilla/vite.config.js
+++ examples/vite-vanilla/vite.config.js
@@ -1,6 +1,9 @@
 export default {
   base: '/gradient-gl/vite-vanilla/',
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    rollupOptions: {
+      external: ['gradient-gl']
+    }
   }
 }
