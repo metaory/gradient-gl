1c9b9b8972ad7b0f15b08b0730be0ed0ba0a27d1
metaory <metaory@gmail.com>
2025-04-27 15:38:28 +0330

    added canvas id

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const createCanvas = (selector = 'body') => {
     ? target
     : target.appendChild(
         Object.assign(document.createElement('canvas'), {
+          id: 'gradient-gl',
           style: 'position:fixed;inset:0;width:100%;height:100%;z-index:-1;',
         }),
       )
